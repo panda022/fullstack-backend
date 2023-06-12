f9d34cff7383f49887dd5721a4284d46e20d9b4b
Pan Gong <pn819328@dal.ca>
2023-06-12 17:50:55 -0300

    Add Spring library document using Open-api

--- src/main/java/com/pangong/fullstackbackendpost/controller/AuthController.java
+++ src/main/java/com/pangong/fullstackbackendpost/controller/AuthController.java
@@ -6,6 +6,9 @@ import com.pangong.fullstackbackendpost.dtos.LoginDto;
 import com.pangong.fullstackbackendpost.dtos.PostDto;
 import com.pangong.fullstackbackendpost.dtos.RegisterDto;
 import com.pangong.fullstackbackendpost.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,9 @@ import java.lang.annotation.Repeatable;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(
+        name = "POST REST APIS for Authentication"
+)
 public class AuthController {
     private AuthService authService;
 
@@ -28,6 +34,14 @@ public class AuthController {
     //Login REST API
     //value ={"",""} multiple url for rest api
     @PostMapping(value = {"/login","/signin"})
+    @Operation(
+            summary = "Login or Signin REST API",
+            description = "This API is used to login the user and set jwt token"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDto loginDto){
         String token = authService.login(loginDto);
         JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
@@ -36,6 +50,14 @@ public class AuthController {
     }
     //Register REST API
     @PostMapping(value = {"/register","/signup"})
+    @Operation(
+            summary = "Register or signup REST API",
+            description = "This API is used to register the user and save the user in the database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     public ResponseEntity<String> login(@RequestBody RegisterDto registerDto){
         String response = authService.register(registerDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
--- src/main/java/com/pangong/fullstackbackendpost/controller/CommentController.java
+++ src/main/java/com/pangong/fullstackbackendpost/controller/CommentController.java
@@ -2,15 +2,23 @@ package com.pangong.fullstackbackendpost.controller;
 
 import com.pangong.fullstackbackendpost.dtos.CommentDto;
 import com.pangong.fullstackbackendpost.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
+@Tag(
+        name = "CRUD REST APIS for Comment"
+)
 public class CommentController {
     private CommentService commentService;
 
@@ -20,19 +28,48 @@ public class CommentController {
 
     //create comment
     @PostMapping("/posts/{postId}/comments")
+    @Operation(
+            summary = "Create Comment REST API",
+            description = "This API is used to save comment into database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
                                                     @Valid @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(commentService.createComment(postId,commentDto), HttpStatus.CREATED);
     }
 
+
     //get comments by postId
     @GetMapping("/posts/{postId}/comments")
+    @Operation(
+            summary = "Get Comment By id REST API",
+            description = "This API is used to get all the comments from the database by postId"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     public List<CommentDto> getCommentsByPostId(@PathVariable(name = "postId")long postId){
         return commentService.getCommentsByPostId(postId);
     }
 
     //get comment by postId and commentId
     @GetMapping("/posts/{postId}/comments/{id}")
+    @Operation(
+            summary = "Get Comment By id REST API",
+            description = "This API is used to get all the comments from the database by postId and commentId"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     public ResponseEntity<CommentDto> getCommentById(@PathVariable(value = "postId") long postId,
                                                      @PathVariable(value = "id") long commentId){
         CommentDto commentDto = commentService.getCommentById(postId, commentId);
@@ -41,6 +78,18 @@ public class CommentController {
 
     //update comment by postId and commentId
     @PutMapping("/posts/{postId}/comments/{id}")
+    @Operation(
+            summary = "Update Comment REST API",
+            description = "This API is used to update specific commnet from the database by postId and commentId"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<CommentDto> updateCommentById(@PathVariable(value = "postId") long postId,
                                                      @PathVariable(value = "id") long commentId,
                                                         @Valid @RequestBody  CommentDto commentDto){
@@ -52,6 +101,18 @@ public class CommentController {
 
     //delete comment by postId and commentId
     @DeleteMapping("/posts/{postId}/comments/{id}")
+    @Operation(
+            summary = "Delete Comment REST API",
+            description = "This API is used to delete specific comment from the database by the postId and commentId"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<String> deleteCommentById(@PathVariable(value = "postId") long postId,
                                   @PathVariable(value = "id") long commentId){
         commentService.deleteComment(postId, commentId);
